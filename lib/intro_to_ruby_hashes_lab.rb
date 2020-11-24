a535e759479bcf95ab576322e53e75cd927fdfd8
Jack Hubert <jphubert93@gmail.com>
2020-11-24 01:25:46 +0000

    Done.

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,6 @@
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  my_hash_creator = {
+  {
     key => value
   }
 end
@@ -21,3 +21,17 @@ def update_counting_hash(hash, key)
   end
   hash
 end
+
+# def update_counting_hash(hash, key)
+#   if hash[key]
+#     hash[key] = hash[key] + 1
+#   else
+#     hash[key] = 1
+#   end
+#   hash
+# end
+
+# def update_counting_hash(hash, key)
+#   hash[key] ? hash[key] += 1 : hash[key] = 1
+#   hash
+# end
